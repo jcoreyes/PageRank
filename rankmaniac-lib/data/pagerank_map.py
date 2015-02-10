9d1583770b982310efa60c8ab49a05e5d692fb81
jcoreyes <jcoreyes@caltech.edu>
2015-02-10 14:24:45 -0800

    Added simple pagerank for uploading

--- rankmaniac-lib/data/pagerank_map.py
+++ rankmaniac-lib/data/pagerank_map.py
@@ -11,28 +11,28 @@ idNumber \t prevRank \t iter \t outLink1,outLink2...
 """
 import sys
 
-# for line in sys.stdin:
-#     lineData = line.rstrip('\n').split('\t')
-#     nodeData = lineData[0].split(':')
-#     currNode = nodeData[1]
-#     rankData = lineData[1].split(',')
-#     currPageRank = float(rankData[0])
-#     # If there's atleast 1 outlink
-#     if len(rankData) > 2:
-#         outLinks = rankData[2:]
-#         numOutLinks = len(outLinks)
-#         for outLink in outLinks:
-#             print("%s\t%f" %(outLink, currPageRank/numOutLinks))
-#     # If no outlinks, then contribution is 1
-#     else:
-#         print("%s\t%f" %(currNode, currPageRank))
-#     # If no iteration number yet start at 1
-#     if len(nodeData) < 3:
-#         iter = 1
-#     else:
-#         iter = int(nodeData[2]) + 1
-#     # prevRank becomes currPageRank
-#     # currPageRank will be recalculated in the next reduce
-#     if len(rankData) < 3:
-#         outLinks = ['no_outlinks']
-#     print ("%s\t%f\t%d\t%s" %(currNode, currPageRank, iter, ','.join(outLinks)))
+for line in sys.stdin:
+    lineData = line.rstrip('\n').split('\t')
+    nodeData = lineData[0].split(':')
+    currNode = nodeData[1]
+    rankData = lineData[1].split(',')
+    currPageRank = float(rankData[0])
+    # If there's atleast 1 outlink
+    if len(rankData) > 2:
+        outLinks = rankData[2:]
+        numOutLinks = len(outLinks)
+        for outLink in outLinks:
+            print("%s\t%f" %(outLink, currPageRank/numOutLinks))
+    # If no outlinks, then contribution is 1
+    else:
+        print("%s\t%f" %(currNode, currPageRank))
+    # If no iteration number yet start at 1
+    if len(nodeData) < 3:
+        iter = 1
+    else:
+        iter = int(nodeData[2]) + 1
+    # prevRank becomes currPageRank
+    # currPageRank will be recalculated in the next reduce
+    if len(rankData) < 3:
+        outLinks = ['no_outlinks']
+    print ("%s\t%f\t%d\t%s" %(currNode, currPageRank, iter, ','.join(outLinks)))
--- rankmaniac-lib/data/process_reduce.py
+++ rankmaniac-lib/data/process_reduce.py
@@ -4,33 +4,33 @@ Input will be of form:
 NodeId:idNumber:iter \t currRank,prevRank,outLink1,outLink2...
 """
 import sys
-# nodes = {}
-# avgRankDiff = 0
-# maxRankDiff = -sys.maxint
-# iter = 0
-# for line in sys.stdin:
-#     lineData = line.split('\t')
-#
-#     nodeData = lineData[0].split(':')
-#     nodeID = int(nodeData[1])
-#     iter = int(nodeData[2])
-#
-#     rankData = lineData[1].split(',')
-#     currRank = float(rankData[0])
-#     prevRank = float(rankData[1])
-#
-#     rankDiff =  abs(currRank - prevRank)
-#     maxRankDiff = max(maxRankDiff, rankDiff)
-#     nodes[nodeID] = {}
-#     nodes[nodeID]['line'] = line
-#     nodes[nodeID]['rankDiff'] = rankDiff
-#     nodes[nodeID]['currRank'] = currRank
+nodes = {}
+avgRankDiff = 0
+maxRankDiff = -sys.maxint
+iter = 0
+for line in sys.stdin:
+    lineData = line.split('\t')
 
-# if iter >= 1:
-    #topRanks = sorted(nodes.items(), key=lambda x:x[1]['currRank'], reverse=True)
-for i in range(20):
-    print("FinalRank:%f\t%d" %(0, 0))
-        #print("FinalRank:%f\t%d" %(topRanks[i][1]['currRank'], topRanks[i][0]))
-# else:
-#     for node, data in nodes.items():
-#         sys.stdout.write(data['line'])
+    nodeData = lineData[0].split(':')
+    nodeID = int(nodeData[1])
+    iter = int(nodeData[2])
+
+    rankData = lineData[1].split(',')
+    currRank = float(rankData[0])
+    prevRank = float(rankData[1])
+
+    rankDiff =  abs(currRank - prevRank)
+    maxRankDiff = max(maxRankDiff, rankDiff)
+    nodes[nodeID] = {}
+    nodes[nodeID]['line'] = line
+    nodes[nodeID]['rankDiff'] = rankDiff
+    nodes[nodeID]['currRank'] = currRank
+
+if iter >= 50:
+    topRanks = sorted(nodes.items(), key=lambda x:x[1]['currRank'], reverse=True)
+    for i in range(20):
+        #sys.stdout.write("FinalRank:%f\t%d\n" %(0, i))
+        print("FinalRank:%f\t%d" %(topRanks[i][1]['currRank'], topRanks[i][0]))
+else:
+    for node, data in nodes.items():
+        sys.stdout.write(data['line'])
